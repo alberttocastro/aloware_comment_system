470949adf5a03ac7ee2aca56e975f546de77efc7
alberttocastro <albertto.programmer@gmail.com>
2022-03-16 13:58:04 -0300

    Adds reflexive relationchip on comments

--- app/Models/comment.php
+++ app/Models/comment.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'body',
+        'parent_comment_id'
+    ];
+
+    public function parent_comment()
+    {
+        return $this->belongsTo(comment::class, 'parent_comment_id', 'id');
+    }
+
+    public function child_comments()
+    {
+        return $this->hasMany(comment::class, 'parent_comment_id', 'id');
+    }
 }
