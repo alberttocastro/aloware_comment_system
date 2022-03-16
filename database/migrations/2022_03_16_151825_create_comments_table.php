a259c57e50728afd1f4f0ce8cad0ad0ee97263d8
alberttocastro <albertto.programmer@gmail.com>
2022-03-16 12:47:35 -0300

    Comment table migration

---
+++ database/migrations/2022_03_16_151825_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->text('body');
+            $table->unsignedBigInteger('parent_comment_id');
+
+            $table->foreign('parent_comment_id')->references('id')->on('comments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
