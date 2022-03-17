b4bd6a3f2eb0ae4229331088d8714b2577893e91
alberttocastro <albertto.programmer@gmail.com>
2022-03-17 00:35:43 -0300

    Creates standard layout

---
+++ resources/js/pages/StandardLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <a href="/" id="title-link"
+        ><v-app-bar-title> The One Post Blog </v-app-bar-title></a
+      >
+    </v-app-bar>
+    <v-main>
+      <slot></slot>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#title-link {
+  text-decoration: none;
+  color: unset;
+}
+</style>
